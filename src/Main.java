0e062f05dd2e75da419d789b2e4bdc134e03e429
user <arzykulova2020@gmail.com>
2023-02-24 20:14:10 +0600

    final

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Author author = new Author("Чынгыз Айтматов", "aitmatovgmail.com", 'м');
-        Author author1 = new Author("Aнтон Чехов", "antongmail.com", 'м');
+        Author author = new Author("Чынгыз Айтматов", "aitmatovgmail.com", 'm');
+        Author author1 = new Author("Aнтон Чехов", "antongmail.com", 'm');
+        Author author2 = new Author("Алыкул Осмонов", "osmonovgmail.com", 'm');
         Book book = new Book("Кылым карытар бир кун", 1000, 0, author);
         Book book1 = new Book("Пересолил", 500, 0, author1);
-        System.out.println(author);
-        System.out.println(author1);
-        System.out.println(book);
-        System.out.println(book1);
+        Book book2 = new Book("Ырлар жыйнагы", 800, 0, author2);
+
+        Book[] books = {book, book1, book2};
+        for (Book book3 : books) {
+            System.out.println(book3);
+
+
+            }
+
+        }
+
     }
-}
